c53754329d8031cc4b9184f54429c028ec3aa2d8
Angelo Grangetto <angelograngetto@gmail.com>
2021-06-20 20:11:12 -0300

    changes

---
+++ src/services/settings.dev.js
@@ -0,0 +1,10 @@
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports.API_URL = exports.API_KEY = void 0;
+var API_KEY = '';
+exports.API_KEY = API_KEY;
+var API_URL = 'https://api.giphy.com/v1/';
+exports.API_URL = API_URL;
